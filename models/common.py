872a87e6934b5195e5664caa3049df12d2c50cc7
aybora <aybora@metu.edu.tr>
2022-01-07 15:29:35 +0300

    recursion corrected

--- models/common.py
+++ models/common.py
@@ -251,11 +251,11 @@ class Conv(nn.Module):
     def forward_fuse(self, x):
         return self.act(self.conv(x))
 
-class ConvLSTM(nn.Module):
+class ConvLSTMLayer(nn.Module):
     # Standard convolution
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
         super().__init__()
-        self.convlstm = ConvLSTM(c1, c2, (3, 3), 1)
+        self.convlstm = ConvLSTM(c1, c2, (k, k), 1)
         self.bn = nn.BatchNorm2d(c2)
         self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
 
